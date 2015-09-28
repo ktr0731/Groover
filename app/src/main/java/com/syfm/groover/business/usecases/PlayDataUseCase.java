d4c64479f5084c40fadcbd7e068f6337d0561155
lycoris0731 <darkright87@gmail.com>
2015-09-29 03:05:54 +0900

    Moved initialize code to PlayDataFragment

--- app/src/main/java/com/syfm/groover/business/usecases/PlayDataUseCase.java
+++ app/src/main/java/com/syfm/groover/business/usecases/PlayDataUseCase.java
@@ -2,6 +2,7 @@ package com.syfm.groover.business.usecases;
 
 import android.util.Log;
 
+import com.syfm.groover.business.entities.EventData;
 import com.syfm.groover.business.entities.PlayData;
 import com.syfm.groover.data.network.ApiClient;
 import com.syfm.groover.data.storage.PlayDataDBController;
@@ -13,43 +14,44 @@ import de.greenrobot.event.EventBus;
 /**
  * Created by lycoris on 2015/09/26.
  */
-public class PlayDataUseCase {
+public class PlayDataUseCase implements ApiClient.PlayDataCallback {
 
     public class PlayDataEvent {
         public final PlayData playData;
         public PlayDataEvent(PlayData playData) {
             this.playData = playData;
         }
+    }
 
+    public class SetPlayData {
+        public final boolean success;
+        public SetPlayData(boolean success) {
+            this.success = success;
+        }
+    }
+
+    public void setPlayData() {
+        ApiClient client = new ApiClient();
+        client.getPlayData(this);
     }
 
     public void getPlayData() {
-
         PlayData playData;
-
         //SQLiteから取得
         PlayDataDBController controller = new PlayDataDBController();
         if(controller.isExistsRecord()) {
-            Log.d("Unko", "SQLiteから取得します");
             playData = controller.getLatest();
             EventBus.getDefault().post(new PlayDataEvent(playData));
-            return;
+      }
+
+    }
+
+    public void isSuccess(Boolean success) {
+        if(success) {
+            EventBus.getDefault().post(new SetPlayData(true));
+        } else {
+            EventBus.getDefault().post(new SetPlayData(false));
         }
-
-        Log.d("Unko", "ApiClientから取得します");
-        ApiClient client = new ApiClient();
-        client.getPlayData(new ApiClient.PlayDataListener() {
-            @Override
-            public void onSuccess(PlayData playData) {
-                EventBus.getDefault().post(new PlayDataEvent(playData));
-            }
-
-            @Override
-            public void onFailure() {
-
-            }
-        });
-
     }
 
 }
--- app/src/main/java/com/syfm/groover/presenters/activities/MainActivity.java
+++ app/src/main/java/com/syfm/groover/presenters/activities/MainActivity.java
@@ -1,6 +1,5 @@
 package com.syfm.groover.presenters.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
@@ -10,12 +9,7 @@ import android.view.MenuItem;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.syfm.groover.R;
-import com.syfm.groover.business.usecases.PlayDataUseCase;
-import com.syfm.groover.data.network.AppController;
-import com.syfm.groover.presenters.fragments.CommonDialogFragment;
 import com.syfm.groover.presenters.fragments.MainFragmentPagerAdapter;
-
-import de.greenrobot.event.EventBus;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,13 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.container);
-        /*
-        if (AppController.getInstance().checkLoginCookie()) {
-            //Go to LoginActivity
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            int code = getResources().getInteger(R.integer.status_code_login);
-            startActivityForResult(intent, code);
-        }*/
 
         ViewPager pager = (ViewPager) findViewById(R.id.main_pager);
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -62,71 +49,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (getResources().getInteger(R.integer.status_code_login) == requestCode) {
-
-            Bundle bundle = new Bundle();
-            bundle.putInt(CommonDialogFragment.FIELD_TITLE, R.string.dialog_title_login);
-            bundle.putInt(CommonDialogFragment.FIELD_MESSAGE, R.string.dialog_title_login);
-            bundle.putInt(CommonDialogFragment.FIELD_LAYOUT, R.layout.dialog_progress);
-            //bundle.putBoolean(CommonDialogFragment.FIELD_PROGRESS_BAR, true);
-
-            final CommonDialogFragment dialogFragment = new CommonDialogFragment();
-            dialogFragment.setArguments(bundle);
-            dialogFragment.show(getSupportFragmentManager(), "getDataDialog");
-
-            android.os.Handler handler = new android.os.Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    PlayDataUseCase useCase = new PlayDataUseCase();
-                    useCase.getPlayData();
-                    dialogFragment.dismiss();
-                }
-            }, 1000);
-            /*handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    ApiClient.getAverageScore();
-                }
-            }, 2000);
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    ApiClient.getMusicDetail();
-                }
-            }, 3000);
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    ApiClient.getShopData();
-                }
-            }, 4000);
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    ApiClient.getStageData();
-                    dialogFragment.dismiss();
-                }
-            }, 5000);*/
-        }
-    }
-
-    @Override
     protected void onStart() {
         super.onStart();
-        EventBus.getDefault().register(this);
     }
 
     @Override
     public void onStop() {
-        EventBus.getDefault().unregister(this);
         super.onStop();
-    }
-
-    public void onEvent(PlayDataUseCase event) {
-
     }
 }
