159cd81fa4418ae8cadd40d931b876897c10191e
lycoris0731 <darkright87@gmail.com>
2015-10-09 16:35:11 +0900

    Created MusicList.xml

---
+++ app/src/main/java/com/syfm/groover/presenters/adapter/MusicListAdapter.java
@@ -0,0 +1,19 @@
+package com.syfm.groover.presenters.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.BaseAdapter;
+
+import com.syfm.groover.business.entities.MusicListEntity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lycoris on 2015/10/09.
+ *//*
+public class MusicListAdapter extends BaseAdapter {
+    Context context;
+    LayoutInflater inflater = null;
+    //ArrayList<MusicList>
+}
+*/
