cc39df08a682252cfc12e5159ce3a7e34775710e
Lycoris <aizu.s1230022@gmail.com>
2016-04-26 22:52:04 +0900

    [Add] Create now-event mock up

--- app/src/main/java/com/syfm/groover/View/fragments/CurrentEventDataFragment.java
+++ app/src/main/java/com/syfm/groover/View/fragments/CurrentEventDataFragment.java
@@ -1,7 +1,6 @@
 package com.syfm.groover.view.fragments;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
