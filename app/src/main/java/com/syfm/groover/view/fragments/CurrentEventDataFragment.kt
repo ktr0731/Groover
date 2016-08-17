ac91b90831972d9152c856387e4e22cb21215308
Lycoris <aizu.s1230022@gmail.com>
2016-08-17 17:05:47 +0900

    [Add] Kotlin settings to Gradle

---
+++ app/src/main/java/com/syfm/groover/view/fragments/CurrentEventDataFragment.kt
@@ -0,0 +1,35 @@
+package com.syfm.groover.view.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.syfm.groover.R
+
+/**
+ * Created by lycoris on 2016/04/23.
+ */
+class CurrentEventDataFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater!!.inflate(R.layout.fragment_current_event_data, container, false)
+        return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+}
